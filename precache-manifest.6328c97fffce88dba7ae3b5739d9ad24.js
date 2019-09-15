f8ae70cf4745e8db5c7ac98e6fe8ee3cf0aef1cc
Grace Akpan <opps4lrng@gmail.com>
2019-09-14 21:18:25 -0600

    Updates

---
+++ precache-manifest.6328c97fffce88dba7ae3b5739d9ad24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac89c29565be2783082e0a1a001e4e8",
+    "url": "/greed-react/index.html"
+  },
+  {
+    "revision": "038a905a1db332dac462",
+    "url": "/greed-react/static/css/main.06f19e8c.chunk.css"
+  },
+  {
+    "revision": "54c6ca79e8d9a91604ea",
+    "url": "/greed-react/static/js/2.341655d5.chunk.js"
+  },
+  {
+    "revision": "038a905a1db332dac462",
+    "url": "/greed-react/static/js/main.bd422a84.chunk.js"
+  },
+  {
+    "revision": "f687ba01e49b28616475",
+    "url": "/greed-react/static/js/runtime~main.53cd4dbc.js"
+  }
+]);
